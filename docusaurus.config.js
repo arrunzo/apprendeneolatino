886f0dcc13b2e3a00554a03ef13f4a1608dc703e
arrunzo <arrunzo1@gmail.com>
2025-01-31 20:25:33 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://apprendeneolatino.com',
+  url: 'https://arrunzo.github.io/apprendeneolatino/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/apprendeneolatino/',
