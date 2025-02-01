1e46235570169d6ba64d3f9c875f7cbdfe38d180
arrunzo <arrunzo1@gmail.com>
2025-01-31 20:38:42 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,13 +18,13 @@ const config = {
   url: 'https://arrunzo.github.io/apprendeneolatino/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/apprendeneolatino/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'arrunzo', // Usually your GitHub org/user name.
-  projectName: 'apprendeneolatino', // Usually your repo name.
+  projectName: '/', // Usually your repo name.
 
   headTags: [
     {
