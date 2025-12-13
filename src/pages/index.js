5da3c608c158c2544f92fb36d7346e41604b0869
arrunzo <arrunzo1@gmail.com>
2025-12-13 14:31:16 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/lessons/pronunciation/alphabet">
+            to="/lessons/pronunciation/alphabet">
             <Translate>Learn now!</Translate>
           </Link>
         </div>
